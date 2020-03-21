9960d93fe3cab59b74b6c7d085feba1b887ca136
RepComm <rccommandotac@gmail.com>
2020-03-20 19:55:50 -0500

    Added click-open-dialog ability to drop area Modified styling a bit

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ on(randKeyButton, "click", (evt) => {
   }
   keyInput.value = bufferToHex(keyBuffer);
 });
+randKeyButton.click();
 
 let fset = (buffer, name = "out.bin") => {
   let element = get("f-out");
@@ -22,7 +23,16 @@ let fset = (buffer, name = "out.bin") => {
   element.click();
   console.log(element);
 }
-
+let fget = (cb) => {
+  /**@type {HTMLInputElement} */
+  let fin = get("f-in");
+  let listener = (evt) => {
+    cb(fin.files);
+    fin.removeEventListener("change", listener);
+  };
+  fin.addEventListener("change", listener);
+  fin.click();
+}
 function bufferToHex(buffer) {
   return Array
     .from(new Uint8Array(buffer))
@@ -30,28 +40,44 @@ function bufferToHex(buffer) {
     .join("");
 }
 
-let dropBox = get("drop-box");
-on(dropBox, "drop", (evt) => {
-  evt.preventDefault();
+function encryptFiles (files) {
+  if (files.length > 0) {
+    let file = files[0];
+    file.arrayBuffer().then((ab)=>{
+      let buf = encryptBuffer(new Uint8Array(ab), keyBuffer);
+      let blob = new Blob([buf], {type: 'application/octet-stream'});
+      let url = URL.createObjectURL(blob);
+      fset(url, file.name);
+    });
+  }
+}
 
+function calcKeyFromInput () {
   keyBuffer = new Uint8Array(keyInput.value.length);
   for (let i = 0; i < keyBuffer.length; i++) {
     keyBuffer[i] = keyInput.value.charCodeAt(i);
   }
+}
+
+let dropBox = get("drop-box");
+on(dropBox, "drop", (evt) => {
+  evt.preventDefault();
+
+  calcKeyFromInput();
 
   if (evt.dataTransfer && evt.dataTransfer.files && evt.dataTransfer.files.length > 0) {
-    evt.dataTransfer.files[0].arrayBuffer().then((ab)=>{
-      let buf = encryptBuffer(new Uint8Array(ab), keyBuffer);
-      let blob = new Blob([buf], {type: 'application/octet-stream'});
-      let url = URL.createObjectURL(blob);
-      fset(url, evt.dataTransfer.files[0].name);
-    });
+    encryptFiles(evt.dataTransfer.files);
   }
 });
 on(dropBox, "dragover", (evt) => {
   evt.preventDefault();
 });
-
+on(dropBox, "click", (evt)=>{
+  calcKeyFromInput();
+  fget((files)=>{
+    encryptFiles(files);
+  });
+});
 /**Encrypt a buffer by xor-ing with another
  * @param {Uint8Array} buffer 
  * @param {Uint8Array} key 
